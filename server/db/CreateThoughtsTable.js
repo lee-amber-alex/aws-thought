b4495fb91cae6cffa336e45e6ef04602b6b8b474
Amber Lee <lee.amber.alex@gmail.com>
2021-03-24 13:02:29 -0700

    Created dynamo table and loaded seed data.

--- server/db/CreateThoughtsTable.js
+++ server/db/CreateThoughtsTable.js
@@ -2,33 +2,45 @@ const AWS = require("aws-sdk");
 
 // config points to local instance,
 // updates local environmental variables
-AWS.config.update({
-  region: "us-east-2",
-  endpoint: "http://localhost:8000"
-});
 
-const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+AWS.config.update({
+    region: "us-eas-2",
+    endpoint: "http://localhost:8000"
+})
+
+const dynamodb = new AWS.DynamoDB({apiVersion: "2012-08-10"})
+
+// Next we'll create a params object that will hold the schema and metadata of the table
 
 const params = {
+    // designate table name
     TableName : "Thoughts",
+    // define the partition key and the sort key
     KeySchema: [       
         { AttributeName: "username", KeyType: "HASH"},  //Partition key
-        { AttributeName: "createdAt", KeyType: "RANGE" }  //Sort key
+        { AttributeName: "createdAt", KeyType: "RANGE" }, /*Sort key */
+       
+        
     ],
+    // defines the attributes we've used for the hash and range keys
     AttributeDefinitions: [       
-        { AttributeName: "username", AttributeType: "S" },
-        { AttributeName: "createdAt", AttributeType: "N" }
+        { AttributeName: "username", AttributeType: "S" }, /*string*/
+        { AttributeName: "createdAt", AttributeType: "N" }, /*number*/
+        
+        
     ],
+    // maximum write and read capacity of the database
     ProvisionedThroughput: {       
         ReadCapacityUnits: 10, 
         WriteCapacityUnits: 10
     }
 };
+
 // create table method using schema params
-dynamodb.createTable(params, (err, data) => {
-    if (err) {
-        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+dynamodb.createTable(params, (err, data) =>{
+    if(err){
+        console.error("Table not created. Error JSON:", JSON.stringify(err, null, 2))
     } else {
-        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+        console.log("Table created. Description JSON:", JSON.stringify(data, null, 2))
     }
-});
+})
--- server/db/LoadThoughts.js
+++ server/db/LoadThoughts.js
@@ -1,15 +1,25 @@
 const AWS = require("aws-sdk");
-const fs = require('fs');
+const fs = require("fs");
 
 AWS.config.update({
-  region: "us-east-2",
-  endpoint: "http://localhost:8000"
+  region: "us-eas-2",
+  endpoint: "http://localhost:8000",
 });
-const dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+
+// DocumentClient() to create the dynamodb service object
+const dynamodb = new AWS.DynamoDB.DocumentClient({ apiVersion: "2012-08-10" });
 
 console.log("Importing thoughts into DynamoDB. Please wait.");
-const allUsers = JSON.parse(fs.readFileSync('./server/seed/users.json', 'utf8'));
-allUsers.forEach(user => {
+
+// The relative path for the fs.readFileSync function 
+// is relative to where the file is executed, not the path between files.
+const allUsers = JSON.parse(
+  fs.readFileSync("./server/seed/users.json", "utf8")
+);
+
+// loop over allUsers to create params object
+
+allUsers.forEach( user =>{
   const params = {
     TableName: "Thoughts",
     Item: {
@@ -18,12 +28,14 @@ allUsers.forEach(user => {
       "thought": user.thought
     }
   };
-
-  dynamodb.put(params, (err, data) => {
-    if (err) {
-      console.error("Unable to add thought", user.username, ". Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-      console.log("PutItem succeeded:", user.username);
+  dynamodb.put(params, (err,data) =>{
+    if(err){
+      console.error("Unable to add thought", user.username,".Error JSON:", JSON.stringify(err, null, 2))
+    } else{
+      console.log("PutItem succeed", user.username)
     }
-  });
+  })
 });
+
+
+
