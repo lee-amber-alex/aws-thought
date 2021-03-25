22a84b881fedd24a6468e579fd5955bc307ef9d0
Amber Lee <lee.amber.alex@gmail.com>
2021-03-25 13:41:00 -0700

    Get, Post and Put routes created.

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,157 +1,92 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+
 const AWS = require("aws-sdk");
 const awsConfig = {
   region: "us-east-2",
   endpoint: "http://localhost:8000",
-
 };
+
 AWS.config.update(awsConfig);
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const table = "Thoughts";
 
 // get all users' thoughts
-router.get('/users', (req, res) => {
-  const params = {
-    TableName: table
-  };
-  dynamodb.scan(params, (err, data) => {
-    if (err) {
-      res.status(500).json(err); // an error occurred
-    } else {
-      res.json(data.Items)
-    }
-  });
-})
-
-// get thoughts from a user
-router.get('/users/:username', (req, res) => {
-  console.log(`Querying for thought(s) from ${req.params.username}.`);
+router.get("/users", (req, res) => {
   const params = {
     TableName: table,
-    KeyConditionExpression: "#un = :user",
-    ExpressionAttributeNames: {
-      "#un": "username",
-      "#ca": "createdAt",
-      "#th": "thought"
-    },
-    ExpressionAttributeValues: {
-      ":user": req.params.username
-    },
-    ProjectionExpression: "#th, #ca"
   };
 
-  dynamodb.query(params, (err, data) => {
+  // return all items in the table
+  dynamodb.scan(params, (err, data) => {
     if (err) {
-      console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-      res.status(500).json(err); // an error occurred
+      res.status(500).json(err);
     } else {
-      console.log("Query succeeded.");
-      res.json(data.Items)
+      res.json(data.Items);
     }
   });
 });
 
-// Create new user
-router.post('/users', (req, res) => {
+// get thoughts from a user
+router.get("/users/:username", (req, res) => {
+  console.log(`Quering for thought(s) from ${req.params.username}.`);
+
+  const params = {
+    TableName: table,
+    // determines which attributes or columns will be returned
+    // similar to SELECT in SQL
+    ProjectionExpression: "#th, #ca",
+    // specifies the search criteria, used to filter the query, using comparison operators(=)
+    // similar to WHERE in SQL
+    KeyConditionExpression: "#un = :user",
+    // defines the aliases used within the KeyConditionExpression (#un = username)
+    ExpressionAttributeNames: {
+      "#un": "username",
+      "#ca": "createdAt",
+      "#th": "thought",
+    },
+    // defines the aliases for the attribute values (:user = #un = username = req.params.username)
+    ExpressionAttributeValues: {
+      ":user": req.params.username,
+    },
+
+    // determines the sort order, default is "true" which is ascending, "false" is descending
+    ScanIndexForward: false,
+  };
+  // returns what's specified in the ProjectExpression. 
+  dynamodb.query(params, (err, data) => {
+    if (err) {
+      console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+      res.status(500).json(err);
+    } else {
+      console.log("Query succeeded.", `${data.Items}`);
+      res.json(data.Items);
+    }
+  });
+});
+
+// create a new thought.
+router.post("/users", (req, res) =>{
+
   const params = {
     TableName: table,
     Item: {
       "username": req.body.username,
       "createdAt": Date.now(),
-      "thought": req.body.thought
-    }
-  };
-  dynamodb.put(params, (err, data) => {
-    if (err) {
-      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-      res.status(500).json(err); // an error occurred
-    } else {
-      console.log("Added item:", JSON.stringify(data));
-      res.json({ "Added": JSON.stringify(data, null, 2) });
-    }
-  });
-});
-// // Create new user
-// router.get('/create', (req, res) => {
-//   const params = {
-//     TableName: table,
-//     Item: {
-//       "username": "Carol Dweck",
-//       "createdAt": 1602018401105,
-//       "thought": "You can suffer the pain of change or suffer remaining the way you are."
-//     }
-//   };
-//   // const params = {
-//   //   TableName: table,
-//   //   Item: {
-//   //     "username": req.body.username,
-//   //     "createdAt": Date.now(),
-//   //     "thought": req.body.text
-//   //   }
-//   // };
-//   dynamodb.put(params, (err, data) => {
-//     if (err) {
-//       console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-//     } else {
-//       console.log("Added item:", JSON.stringify(data, null, 2));
-//     }
-//   });
-// });
-
-// Destroy
-router.delete('/users/:time/:username', (req, res) => {
-
-  const username = "Ray Davis"
-  const time = 1602466687289;
-  const thought = "Tolerance only for those who agree with you is no tolerance at all.";
-
-  const params = {
-    TableName: table,
-    Key: {
-      "username": username,
-      "createdAt": time,
-    },
-    KeyConditionExpression: "#ca = :time",
-    ExpressionAttributeNames: {
-      "#ca": "createdAt"
-    },
-    ExpressionAttributeValues: {
-      ":time": time,
+      "thought": req.body.thought,
     }
   }
-
-  console.log("Attempting a conditional delete...");
-  dynamodb.delete(params, (err, data) => {
-    if (err) {
-      console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
-      res.status(500).json(err); // an error occurred
-    } else {
-      console.log("DeleteItem succeeded:", JSON.stringify(data, null, 2));
-    }
-  });
+// Add item to the Thoughts table.
+dynamodb.put(params, (err, data) =>{
+  if(err){
+    console.error("Unable to add new entry. Error JSON:", JSON.stringify(err, null, 2));
+    res.status(500).json(err);
+  } else{
+    console.log("Thought added successfully. Item:", JSON.stringify(data, null, 2));
+    res.json({"Added:": JSON.stringify(data, null, 2)})
+  }
 });
 
-// // update
-// router.put('/users/:username', (req, res) => {
-//   res.json({ "which": "which" })
-// });
-  // const { time, username } = req.params;
+});
 
-//   var table = "Movies";
-
-// var year = 2015;
-// var title = "The Big New Movie";
-
-// var params = {
-//     TableName:table,
-//     Key:{
-//         "year": year,
-//         "title": title
-//     },
-//     ConditionExpression:"info.rating <= :val",
-//     ExpressionAttributeValues: {
-//         ":val": 5.0
-//     }
-// };
 module.exports = router;
